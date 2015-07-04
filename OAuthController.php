cb9e778cec101d3a5952bea12740108727197451
ssnukala <srinivasnukala@SRINIVASs-MBP.home>
2015-07-04 03:25:44 -0500

    Fixed Registration
    Also added language directory, need to activate adding messages using
    plugin

--- OAuthController.php
+++ OAuthController.php
@@ -14,6 +14,7 @@ namespace UserFrosting\OAuth;
 abstract class OAuthController extends \UserFrosting\BaseController {
 
     protected $_provider;       // An OAuth2 provider object
+    protected $_user_profile;       // An profile object returned by the provider
 
 /**
  * constructor
@@ -31,6 +32,151 @@ abstract class OAuthController extends \UserFrosting\BaseController {
 
     }
     
+
+    public function ufRegister(){
+        // POST: user_name, display_name, email, title, password, passwordc, captcha, spiderbro, csrf_token
+        $post = $this->_app->request->post();
+        
+        // Get the alert message stream
+        $ms = $this->_app->alerts; 
+        
+        // Check the honeypot. 'spiderbro' is not a real field, it is hidden on the main page and must be submitted with its default value for this to be processed.
+        if (!$post['spiderbro'] || $post['spiderbro'] != "http://"){
+            error_log("Possible spam received:" . print_r($this->_app->request->post(), true));
+            $ms->addMessage("danger", "Aww hellllls no!");
+            $this->_app->halt(500);     // Don't let on about why the request failed ;-)
+        }  
+               
+        // Load the request schema
+        $requestSchema = new \Fortress\RequestSchema($this->_app->config('schema.path') . "/forms/register.json");
+                   
+        // Set up Fortress to process the request
+        $rf = new \Fortress\HTTPRequestFortress($ms, $requestSchema, $post);        
+
+        // Security measure: do not allow registering new users until the master account has been created.        
+        if (!\UserFrosting\UserLoader::exists($this->_app->config('user_id_master'))){
+            $ms->addMessageTranslated("danger", "MASTER_ACCOUNT_NOT_EXISTS");
+            $this->_app->halt(403);
+        }
+          
+        // Check if registration is currently enabled
+        if (!$this->_app->site->can_register){
+            $ms->addMessageTranslated("danger", "ACCOUNT_REGISTRATION_DISABLED");
+            $this->_app->halt(403);
+        }
+          
+        // Prevent the user from registering if he/she is already logged in
+        if(!$this->_app->user->isGuest()) {
+            $ms->addMessageTranslated("danger", "ACCOUNT_REGISTRATION_LOGOUT");
+            $this->_app->halt(200);
+        }
+                
+        // Sanitize data
+        $rf->sanitize();
+                
+        // Validate, and halt on validation errors.
+        $error = !$rf->validate(true);
+        
+        // Get the filtered data
+        $data = $rf->data();        
+
+        // Check captcha, if required
+        if ($this->_app->site->enable_captcha == "1"){
+            if (!$data['captcha'] || md5($data['captcha']) != $_SESSION['userfrosting']['captcha']){
+                $ms->addMessageTranslated("danger", "CAPTCHA_FAIL");
+                $error = true;
+            }
+        }
+        
+        // Remove captcha, password confirmation from object data
+        $rf->removeFields(['captcha', 'passwordc']);
+        
+        // Perform desired data transformations.  Is this a feature we could add to Fortress?
+        $data['user_name'] = strtolower(trim($data['user_name']));
+        $data['display_name'] = trim($data['display_name']);
+        $data['email'] = strtolower(trim($data['email']));
+        $data['locale'] = $this->_app->site->default_locale;
+        
+        if ($this->_app->site->require_activation)
+            $data['active'] = 0;
+        else
+            $data['active'] = 1;
+        
+        // Check if username or email already exists
+        if (\UserFrosting\UserLoader::exists($data['user_name'], 'user_name')){
+            $ms->addMessageTranslated("danger", "ACCOUNT_USERNAME_IN_USE", $data);
+            $error = true;
+        }
+
+        if (\UserFrosting\UserLoader::exists($data['email'], 'email')){
+            $ms->addMessageTranslated("danger", "ACCOUNT_EMAIL_IN_USE", $data);
+            $error = true;
+        }
+        
+        // Halt on any validation errors
+        if ($error) {
+            $this->_app->halt(400);
+        }
+    
+        // Get default primary group (is_default = GROUP_DEFAULT_PRIMARY)
+        $primaryGroup = \UserFrosting\GroupLoader::fetch(GROUP_DEFAULT_PRIMARY, "is_default");
+        $data['primary_group_id'] = $primaryGroup->id;
+        // Set default title for new users
+        $data['title'] = $primaryGroup->new_user_title;
+        // Hash password
+        $data['password'] = \UserFrosting\Authentication::hashPassword($data['password']);
+        
+        // Create the user
+        $user = new \UserFrosting\User($data);
+
+        // Add user to default groups, including default primary group
+        $defaultGroups = \UserFrosting\GroupLoader::fetchAll(GROUP_DEFAULT, "is_default");
+        $user->addGroup($primaryGroup->id);
+        foreach ($defaultGroups as $group_id => $group)
+            $user->addGroup($group_id);    
+        
+        // Store new user to database
+        $user->store();
+        if ($this->_app->site->require_activation) {
+            // Create and send activation email
+
+            $mail = new \PHPMailer;
+            
+            $mail->From = $this->_app->site->admin_email;
+            $mail->FromName = $this->_app->site->site_title;
+            $mail->addAddress($user->email);     // Add a recipient
+            $mail->addReplyTo($this->_app->site->admin_email, $this->_app->site->site_title);
+            
+            $mail->Subject = $this->_app->site->site_title . " - please activate your account";
+            $mail->Body    = $this->_app->view()->render("common/mail/activate-new.html", [
+                "user" => $user
+            ]);
+            
+            $mail->isHTML(true);                                  // Set email format to HTML
+            
+            if(!$mail->send()) {
+                $ms->addMessageTranslated("danger", "MAIL_ERROR");
+                error_log('Mailer Error: ' . $mail->ErrorInfo);
+// Srinivas : Should we be halting the registraiton process if the email could not be sent out
+// CAn we just give a message that a confirmation could not be sent out, contact the site admin
+// Because at this point the user record is already created. And the user should be able to login
+// Halting it here, does not let the process proceed with the OpenAuthentication, so now the user is stuck
+// with just UF account and does not have a link to the Open Authentication. 
+// Would be good to exit with a warning
+//                 
+//                $this->_app->halt(500);
+            }
+
+            // Activation required
+            $ms->addMessageTranslated("success", "ACCOUNT_REGISTRATION_COMPLETE_TYPE2");
+        } else
+            // No activation required
+            $ms->addMessageTranslated("success", "ACCOUNT_REGISTRATION_COMPLETE_TYPE1");
+
+// Srinivas : The OAuth function will need the user object, so that it can get the ID to save the OAuth record
+// Invoking this in OAuth to register using         
+        return $user;
+    }
     
     // This function should automatically login a user who has been authenticated by the OAuth provider
     public function login(){
--- OAuthControllerLinkedIn.php
+++ OAuthControllerLinkedIn.php
@@ -45,39 +45,52 @@ class OAuthControllerLinkedIn extends OAuthController {
     public function login(){
         // Authenticate 
         $user_details = $this->authenticate();
-    
+//print_r($user_details);
+//die();
         // Load the OAuthUser object for the given uid
 //        $oauth_user = OAuthUserLoader::fetch($user_details->uid, 'uid', 'linkedin');
         $oauth_user = OAuthUserLoader::fetch($user_details->uid, 'uid');
-        
+//print_r($oauth_user);        
         // TODO: check that the user exists, and is not already logged in
         
         // Now get the UF user object and log the user in
-        $_SESSION["userfrosting"]["user"] = \UserFrosting\UserLoader::fetch($oauth_user->user_id);
-        $this->_app->user = $_SESSION["userfrosting"]["user"];
-        $this->_app->user->login();
+        if($oauth_user!== false)
+        {
+            $_SESSION["userfrosting"]["user"] = \UserFrosting\UserLoader::fetch($oauth_user->user_id);
+            $this->_app->user = $_SESSION["userfrosting"]["user"];
+            $this->_app->user->login();
+        }
+        else
+        {
+//            $this->_app->alerts->addMessageTranslated("danger", "OAUTH_NOTCONNECTED",["provider"=>"LinkedIn"]);
+            $this->_app->alerts->addMessageTranslated("danger", "Your LinkedIn Account is not connected to a local account. Plase register using LinkedIn first.",["provider"=>"LinkedIn"]);
+            
+        }
         $this->_app->redirect($this->_app->urlFor('uri_home'));
+            
     }
     
     // Register a user by authenticating via LinkedIn.
     public function register(){
         // Authenticate OAuth
-        $user_details_obj = $this->authenticate();
+//        $user_details_obj = $this->authenticate();
         // TODO: validate submitted fields and what-not
-
+//        $this->_user_profile=$user_details_obj;
         // Create a new User object from the posted data
-        $user = new \UserFrosting\User($data);
-        
+//        $user = new \UserFrosting\User($data);
+//die("Line 71 before saving the user");        
         // Save to database
-        $user->store();
-        
+//        $user->store();
+        $user= $this->ufRegister();
+        $this->_user_profile = $_SESSION['userfrosting']['oauth_details'];
         // Create a new OAuthUser object
-        $user_details = $this->transform($user_details_obj);
+        $user_details = $this->transform($this->_user_profile);
         $user_details['user_id'] = $user->id;
         $user_details['provider'] = "linkedin";
         
         $oauth_user = new OAuthUser($user_details);
-        
+//print_r($user_details);
+//echo("Line 82 storing oauth user");
         // Save to database
         $oauth_user->store();
     }
@@ -86,29 +99,70 @@ class OAuthControllerLinkedIn extends OAuthController {
     {
         $user_details_obj = $this->authenticate();
         
+        $schema = new \Fortress\RequestSchema($this->_app->config('schema.path') . "/forms/register.json");
+        $validators = new \Fortress\ClientSideValidator($schema, $this->_app->translator);                
+
+        $settings = $this->_app->site;
+        
+        // If registration is disabled, send them back to the home page with an error message
+        if (!$settings->can_register){
+            $this->_app->alerts->addMessageTranslated("danger", "ACCOUNT_REGISTRATION_DISABLED");
+            $this->_app->redirect('login');
+        }
+
+//        $_SESSION["userfrosting"]['oauth_controller']=$this;
+        
         $this->_app->render('oauth_register.html', [
             'page' => [
                 'author' =>         $this->_app->site->author,
                 'title' =>          "OAuth Registration",
                 'description' =>    "Registration using OAuth",
-                'alerts' =>         $this->_app->alerts->getAndClearMessages()
+                'alerts' =>         $this->_app->alerts->getAndClearMessages(), 
+                'active_page' =>    "account/register"                
             ],
+            'captcha_image' =>  $this->generateCaptcha(),
+            'validators' => $validators->formValidationRulesJson(),
                 'oauth_details'=>$user_details_obj
-        ]);                  
+        ]);
+//        $this->_app->render('oauth_register.html', [
+//            'page' => [
+//                'author' =>         $this->_app->site->author,
+//                'title' =>          "OAuth Registration",
+//                'description' =>    "Registration using OAuth",
+//                'alerts' =>         $this->_app->alerts->getAndClearMessages()
+//            ],
+//                'captcha_image' =>  $this->generateCaptcha(),
+//                'oauth_details'=>$user_details_obj
+//        ]);                  
     }
     
+    public function pageConfirmOAuth()
+    {
+        $user_details_obj = $this->authenticate();
+//print_r($user_details_obj);        
+        $this->_app->render('oauth_confirm.html', [
+            'page' => [
+                'author' =>         $this->_app->site->author,
+                'title' =>          "LinkedIn Confirmation",
+                'description' =>    "LinkedIn authentication successful",
+                'alerts' =>         $this->_app->alerts->getAndClearMessages()
+            ],
+                'oauth_details'=>$this->_user_profile,
+                'oauth_data'=>$user_details_obj
+        ]);                  
+    }
     // This function should authenticate the user with LinkedIn and return the LinkedIn profile data for that user.
     private function authenticate(){
         $var_getarr = $this->_app->request->get();
-        
+//print_r($var_getarr);        
         $ms = $this->_app->alerts;
         
         // Check given state against previously stored one to mitigate CSRF attack
-        if (empty($var_getarr['state']) || ($var_getarr['state'] !== $_SESSION['oauth2state'])) {
-            $this->app->alerts->addMessage('danger', 'Invalid or missing CSRF token.');
-            unset($_SESSION['oauth2state']);
-            $this->_app->halt(401);
-        }      
+//        if (empty($var_getarr['state']) || ($var_getarr['state'] !== $_SESSION['oauth2state'])) {
+//            $this->_app->alerts->addMessage('danger', 'Invalid Authorization State.('.$var_getarr['state']." : ".$_SESSION['oauth2state'].")");
+//            unset($_SESSION['oauth2state']);
+//            $this->_app->halt(401);
+//        }  
         
         // Try to get an access token (using the authorization code grant)
         $token = $this->_provider->getAccessToken('authorization_code', [
@@ -116,7 +170,9 @@ class OAuthControllerLinkedIn extends OAuthController {
         ]);
 
         // We got an access token, so return the user's details
-        $_SESSION['userfrosting']['oauth_details']=$this->_provider->getUserDetails($token);
+        $this->_user_profile=$this->_provider->getUserDetails($token);
+        $_SESSION['userfrosting']['oauth_details']= $this->_user_profile;
+        
         return $_SESSION['userfrosting']['oauth_details'];
     }
     
